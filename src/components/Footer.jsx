91ab3fe9b80b42e4b229497a347f8b6c3c5ce66b
pivnati <natanmuleta911@gmail.com>
2024-09-03 19:10:29 -0700

    make the footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+      <footer className="bg-gray-800 p-4 text-white text-center">
+        © 2024 Food Delivery, All Rights Reserved.
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
