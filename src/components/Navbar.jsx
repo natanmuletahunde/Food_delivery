d9b1d91d0957066e2882ba45b43f24ac229eb9eb
pivnati <natanmuleta911@gmail.com>
2024-09-03 19:15:30 -0700

    modify the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,15 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav className="bg-blue-500 p-4 text-white">
-      <ul className="flex justify-between">
+    <nav className="bg-blue-500 p-4 text-white flex items-center">
+      <div className="flex-shrink-0">
+        <img
+          src="https://your-image-url.com/food-delivery-icon.png"
+          alt="Food Delivery"
+          className="h-8 w-8"
+        />
+      </div>
+      <ul className="flex ml-auto space-x-6">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/menu">Menu</Link></li>
         <li><Link to="/about">About</Link></li>
