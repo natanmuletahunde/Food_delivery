baddb3f8eb4ac36957acd1cdf3698a1d174349f5
pivnati <natanmuleta911@gmail.com>
2024-09-03 19:17:56 -0700

    modify the  footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,17 @@
-function Footer() {
-    return (
-      <footer className="bg-gray-800 p-4 text-white text-center">
-        © 2024 Food Delivery, All Rights Reserved.
-      </footer>
-    );
-  }
-  
-  export default Footer;
-  
+import Navbar from './Navbar';
+import Footer from './Footer';
+import { Outlet } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow">
+        <Outlet /> {/* This will render the current route's component */}
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
